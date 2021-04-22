cb36b70342470b1bc6529ab080dc09cf5ac104e7
jackq <vincentiss@163.com>
2021-04-22 11:40:45 +0800

    api about alert setting

---
+++ application/btcpoolclient/alert.go
@@ -0,0 +1,134 @@
+package btcpoolclient
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AlertSetting struct {
+	HashrateValue  string `json:"hashrate_value"`
+	HashrateUnit   string `json:"hashrate_unit"`
+	HashrateSuffix string `json:"hashrate_suffix"`
+	HashrateAlert  string `json:"hashrate_alert"`
+	MinerValue     string `json:"miner_value"`
+	MinerAlert     string `json:"miner_alert"`
+	AlertInterval  string `json:"alert_interval"`
+}
+
+func AlertSettings(c *gin.Context, params interface{}) (map[string]AlertSetting, error) {
+	var dest = struct {
+		BtcpoolRescomm
+		Data map[string]AlertSetting `json:"data"`
+	}{}
+
+	_, err := doRequest(c, "alert.settings", params, &dest)
+	if err != nil {
+		return nil, fmt.Errorf("error alert.settings: %v", err)
+	}
+	return dest.Data, nil
+}
+
+type AlertContact struct {
+	Id         string `json:"id"`
+	Uid        string `json:"uid"`
+	Puid       string `json:"puid"`
+	Note       string `json:"note"`
+	Email      string `json:"email"`
+	RegionCode string `json:"region_code"`
+	Phone      string `json:"phone"`
+	IsDefault  string `json:"is_default"`
+	CreatedAt  string `json:"created_at"`
+	UpdatedAt  string `json:"updated_at"`
+}
+
+func AlertContacts(c *gin.Context, params interface{}) (map[string][]AlertContact, error) {
+	var dest = struct {
+		BtcpoolRescomm
+		Data map[string][]AlertContact `json:"data"`
+	}{}
+
+	_, err := doRequest(c, "alert.contacts", params, &dest)
+	if err != nil {
+		return nil, fmt.Errorf("error alert.contacts: %v", err)
+	}
+	return dest.Data, nil
+}
+
+func UpdateAlertHashrate(c *gin.Context, params interface{}) (map[string]interface{}, error) {
+	var dest = struct {
+		BtcpoolRescomm
+		Data map[string]interface{} `json:"data"`
+	}{}
+
+	_, err := doRequest(c, "alert.hashrateUpdate", params, &dest)
+	if err != nil {
+		return nil, fmt.Errorf("error alert.hashrateUpdate: %v", err)
+	}
+	return dest.Data, nil
+}
+
+func UpdateAlertMiners(c *gin.Context, params interface{}) (map[string]interface{}, error) {
+	var dest = struct {
+		BtcpoolRescomm
+		Data map[string]interface{} `json:"data"`
+	}{}
+
+	_, err := doRequest(c, "alert.minersUpdate", params, &dest)
+	if err != nil {
+		return nil, fmt.Errorf("error alert.minersUpdate: %v", err)
+	}
+	return dest.Data, nil
+}
+
+func UpdateAlertInterval(c *gin.Context, params interface{}) (map[string]interface{}, error) {
+	var dest = struct {
+		BtcpoolRescomm
+		Data map[string]interface{} `json:"data"`
+	}{}
+
+	_, err := doRequest(c, "alert.intervalUpdate", params, &dest)
+	if err != nil {
+		return nil, fmt.Errorf("error alert.intervalUpdate: %v", err)
+	}
+	return dest.Data, nil
+}
+
+func DeleteAlertContact(c *gin.Context, params interface{}) (map[string]interface{}, error) {
+	var dest = struct {
+		BtcpoolRescomm
+		Data map[string]interface{} `json:"data"`
+	}{}
+
+	_, err := doRequest(c, "alert.contactDelete", params, &dest)
+	if err != nil {
+		return nil, fmt.Errorf("error alert.contactDelete: %v", err)
+	}
+	return dest.Data, nil
+}
+
+func CreateAlertContact(c *gin.Context, params interface{}) (map[string]interface{}, error) {
+	var dest = struct {
+		BtcpoolRescomm
+		Data map[string]interface{} `json:"data"`
+	}{}
+
+	_, err := doRequest(c, "alert.contactCreate", params, &dest)
+	if err != nil {
+		return nil, fmt.Errorf("error alert.contactCreate: %v", err)
+	}
+	return dest.Data, nil
+}
+
+func UpdateAlertContact(c *gin.Context, params interface{}) (map[string]interface{}, error) {
+	var dest = struct {
+		BtcpoolRescomm
+		Data map[string]interface{} `json:"data"`
+	}{}
+
+	_, err := doRequest(c, "alert.contactUpdate", params, &dest)
+	if err != nil {
+		return nil, fmt.Errorf("error alert.contactUpdate: %v", err)
+	}
+	return dest.Data, nil
+}
---
+++ application/controller/alert.go
@@ -0,0 +1,150 @@
+package controller
+
+import (
+	"btc-pool-appserver/application/btcpoolclient"
+	"btc-pool-appserver/application/library/errs"
+	"btc-pool-appserver/application/library/output"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetAlertSetting(c *gin.Context) {
+	var params AccountParams
+	if err := c.ShouldBindJSON(&params); err != nil {
+		output.ShowErr(c, errs.ApiErrParams)
+		return
+	}
+	if d, err := btcpoolclient.AlertSettings(c, params); err != nil {
+		output.ShowErr(c, err)
+		return
+	} else {
+		output.Succ(c, d)
+	}
+}
+
+func GetAlertContacts(c *gin.Context) {
+	var params AccountParams
+	if err := c.ShouldBindJSON(&params); err != nil {
+		output.ShowErr(c, errs.ApiErrParams)
+		return
+	}
+	if d, err := btcpoolclient.AlertContacts(c, params); err != nil {
+		output.ShowErr(c, err)
+		return
+	} else {
+		output.Succ(c, d)
+	}
+}
+
+func UpdateAlertHashrate(c *gin.Context) {
+	var params struct {
+		AccountParams
+		Hashrate string `json:"hashrate" binding:"required"`
+		Unit     string `json:"unit" binding:"required"`
+		Enabled  string `json:"enabled" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&params); err != nil {
+		output.ShowErr(c, errs.ApiErrParams)
+		return
+	}
+	if d, err := btcpoolclient.UpdateAlertHashrate(c, params); err != nil {
+		output.ShowErr(c, err)
+		return
+	} else {
+		output.Succ(c, d)
+	}
+}
+
+func UpdateAlertMiners(c *gin.Context) {
+	var params struct {
+		AccountParams
+		Miners  string `json:"miners" binding:"required"`
+		Enabled string `json:"enabled" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&params); err != nil {
+		output.ShowErr(c, errs.ApiErrParams)
+		return
+	}
+	if d, err := btcpoolclient.UpdateAlertMiners(c, params); err != nil {
+		output.ShowErr(c, err)
+		return
+	} else {
+		output.Succ(c, d)
+	}
+}
+
+func UpdateAlertInterval(c *gin.Context) {
+	var params struct {
+		AccountParams
+		Interval string `json:"interval" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&params); err != nil {
+		output.ShowErr(c, errs.ApiErrParams)
+		return
+	}
+	if d, err := btcpoolclient.UpdateAlertInterval(c, params); err != nil {
+		output.ShowErr(c, err)
+		return
+	} else {
+		output.Succ(c, d)
+	}
+}
+
+func DeleteAlertContact(c *gin.Context) {
+	var params struct {
+		AccountParams
+		Id string `json:"id" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&params); err != nil {
+		output.ShowErr(c, errs.ApiErrParams)
+		return
+	}
+	if d, err := btcpoolclient.DeleteAlertContact(c, params); err != nil {
+		output.ShowErr(c, err)
+		return
+	} else {
+		output.Succ(c, d)
+	}
+}
+
+func CreateAlertContact(c *gin.Context) {
+	var params struct {
+		AccountParams
+		Note        string `json:"note" binding:"required"`
+		Email       string `json:"email" binding:"required"`
+		Region_code string `json:"region_code" binding:"required"`
+		Phone       string `json:"phone" binding:"required"`
+		Country     string `json:"country" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&params); err != nil {
+		output.ShowErr(c, errs.ApiErrParams)
+		return
+	}
+	if d, err := btcpoolclient.CreateAlertContact(c, params); err != nil {
+		output.ShowErr(c, err)
+		return
+	} else {
+		output.Succ(c, d)
+	}
+}
+
+func UpdateAlertContact(c *gin.Context) {
+	var params struct {
+		AccountParams
+		Note        string `json:"note" binding:"required"`
+		Email       string `json:"email" binding:"required"`
+		Region_code string `json:"region_code" binding:"required"`
+		Phone       string `json:"phone" binding:"required"`
+		Id          string `json:"id" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&params); err != nil {
+		output.ShowErr(c, errs.ApiErrParams)
+		return
+	}
+	if d, err := btcpoolclient.UpdateAlertContact(c, params); err != nil {
+		output.ShowErr(c, err)
+		return
+	} else {
+		output.Succ(c, d)
+	}
+}
