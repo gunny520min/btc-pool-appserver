663e011abcaec32a95884483458eeedff4343f15
jackq <vincentiss@163.com>
2021-05-12 14:50:15 +0800

    modify home coin

--- application/model/homeCoinInfo.go
+++ application/model/homeCoinInfo.go
@@ -2,46 +2,68 @@ package model
 
 import (
 	"btc-pool-appserver/application/btcpoolclient"
-	"github.com/shopspring/decimal"
+
+	"fmt"
 	"math"
+	"strconv"
 	"strings"
+
+	"github.com/shopspring/decimal"
 )
 
 type HomeCoinInfo struct {
-	Coin              string  `json:"coin"`
-	CurrencyUsd       string  `json:"currencyUsd"`       // 币值
-	CurrencyCny       string  `json:"currencyCny"`       // 币值rmb
-	PoolHashrate      string  `json:"poolHashrate"`      // 总算力
-	HashrateUnit      string  `json:"hashrateUnit"`      // 算力单位
-	TotalCount        string  `json:"totalCount"`        // 总币数
-	TotalBlocks       string  `json:"totalBlocks"`       // 总区块数
-	AllHashrate       string  `json:"allHashrate"`       // 全网算力
-	AllHashrateUnit   string  `json:"allHashrateUnit"`   // 全网算力单位
-	Diff              float64 `json:"diff"`              // 全网难度
-	NextDiff          string  `json:"nextDiff"`          // 下次调整难度
-	NextDiffTime      string  `json:"nextDiffTime"`      // 下次调整难度时间
-	IncomeCoin        string  `json:"incomeCoin"`        // 币单位收益
-	IncomeUnit        string  `json:"incomeUnit"`        // 单位收益单位： th/s
-	IncomeCurrencyUsd string  `json:"incomeCurrencyUsd"` // 币值单位收益
-	IncomeCurrencyCny string  `json:"incomeCurrencyCny"` // 币值单位收益rmb
-	// DiffUnit        string `json:"diffUnit"`        // 全网难度单位
-	// NextDiffPer     string  `json:"nextDiffPer"`     // 下次调整难度百分比
+	Coin              string `json:"coin"`
+	CurrencyUsd       string `json:"currencyUsd"`       // 币值
+	CurrencyCny       string `json:"currencyCny"`       // 币值rmb
+	PoolHashrate      string `json:"poolHashrate"`      // 总算力
+	HashrateUnit      string `json:"hashrateUnit"`      // 算力单位
+	TotalCount        string `json:"totalCount"`        // 总币数
+	TotalBlocks       string `json:"totalBlocks"`       // 总区块数
+	AllHashrate       string `json:"allHashrate"`       // 全网算力
+	AllHashrateUnit   string `json:"allHashrateUnit"`   // 全网算力单位
+	Diff              string `json:"diff"`              // 全网难度
+	NextDiff          string `json:"nextDiff"`          // 下次调整难度
+	NextDiffChange    string `json:"nextDiffChange"`    // 下次调整难度百分比
+	NextDiffTime      string `json:"nextDiffTime"`      // 下次调整难度时间
+	IncomeCoin        string `json:"incomeCoin"`        // 币单位收益
+	IncomeUnit        string `json:"incomeUnit"`        // 单位收益单位： th/s
+	IncomeCurrencyUsd string `json:"incomeCurrencyUsd"` // 币值单位收益
+	IncomeCurrencyCny string `json:"incomeCurrencyCny"` // 币值单位收益rmb
+
 }
 
 func (info *HomeCoinInfo) SetData(statInfo btcpoolclient.CoinStat, income btcpoolclient.CoinIncome) {
 	info.Coin = statInfo.Coin_type
+
 	info.TotalCount = keepStringNum(statInfo.Rewards_count, 0)
 	info.TotalBlocks = keepStringNum(statInfo.Blocks_count, 0)
 	info.PoolHashrate = keepStringNum(statInfo.Stats.Shares.Shares_15m, 3)
+
 	info.HashrateUnit = statInfo.Stats.Shares.Shares_unit + statInfo.Coin_suffix
 
 	var hash, unit = calculateHashRate(income.Hashrate, 3)
 	info.AllHashrate = hash
 	info.AllHashrateUnit = unit + statInfo.Coin_suffix
-	info.Diff = income.Diff
-	info.NextDiff = income.NextDiff
-	info.NextDiffTime = income.DiffAdjustTime
+
 	info.IncomeUnit = income.IncomeHashrateUnit + income.IncomeHashrateUnitSuffix
+
+	var diff, diffUnit = calculateHashRate(income.Diff, 2)
+	info.Diff = fmt.Sprintf("%v %v", diff, diffUnit)
+
+	if strings.ToLower(statInfo.Coin_type) == "btc" {
+		if nDiff, err := strconv.ParseFloat(income.NextDiff, 64); err != nil {
+			change := ((nDiff - income.Diff) / income.Diff) * 100
+			var nDiffStr, nDiffUnit = calculateHashRate(nDiff, 2)
+			info.NextDiff = fmt.Sprintf("%v %v", nDiffStr, nDiffUnit)
+			info.NextDiffChange = fmt.Sprintf("%0.3f%%", change)
+		}
+		info.NextDiffTime = income.DiffAdjustTime
+	} else {
+		info.NextDiff = income.NextDiff
+		info.NextDiffTime = income.DiffAdjustTime
+		info.NextDiffChange = ""
+
+	}
 
 	if strings.ToLower(statInfo.Coin_type) == "btc" && strings.ToLower(income.PaymentMode) == "fpps" {
 		info.IncomeCoin = keepFloatNum(income.IncomeOptimizeCoin, 8)
@@ -52,7 +74,8 @@ func (info *HomeCoinInfo) SetData(statInfo btcpoolclient.CoinStat, income btcpoo
 		info.IncomeCurrencyCny = keepFloatNum(income.IncomeCny, 2)
 		info.IncomeCurrencyUsd = keepFloatNum(income.IncomeUsd, 2)
 	}
-
+	info.CurrencyCny = fmt.Sprintf("%.2f", income.IncomeCny/income.IncomeCoin)
+	info.CurrencyUsd = fmt.Sprintf("%.2f", income.IncomeUsd/income.IncomeCoin)
 }
 
 func keepStringNum(value string, l int32) string {
