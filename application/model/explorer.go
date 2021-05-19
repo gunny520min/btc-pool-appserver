2d36b9a460b1fda8f91dc7edaac599496630313e
jackq <vincentiss@163.com>
2021-05-19 18:21:47 +0800

    modify file name style

--- application/model/explorer.go
+++ application/model/explorer.go
@@ -4,18 +4,18 @@ type LatestBlock struct {
 	Timestamp string `json:"timestamp"`
 	Reward    string `json:"reward"`
 	Height    int    `json:"height"`
-	PoolName  string `json:"pool_name"`
+	PoolName  string `json:"poolName"`
 	Hash      string `json:"hash"`
 	Size      string `json:"size"`
 }
 
 type PoolRank struct {
-	PoolName               string `json:"pool_name"`
-	IconLink               string `json:"icon_link"`
-	RealtimeHashrate       string `json:"realtime_hashrate"`
-	EstimateHashrate       string `json:"estimate_hashrate"`
-	RealtimeCur2maxPercent string `json:"realtime_cur2max_percent"`
-	EstimateCur2max        string `json:"estimate_cur2max"`
-	HashSuffix             string `json:"hashrate_suffix"`
-	RealtimeDiff24hPercent string `json:"realtime_diff_24h_percent"`
+	PoolName               string `json:"poolName"`
+	IconLink               string `json:"iconLink"`
+	RealtimeHashrate       string `json:"realtimeHashrate"`
+	EstimateHashrate       string `json:"estimateHashrate"`
+	RealtimeCur2maxPercent string `json:"realtimeCur2maxPercent"`
+	EstimateCur2max        string `json:"estimateCur2max"`
+	HashSuffix             string `json:"hashrateSuffix"`
+	RealtimeDiff24hPercent string `json:"realtimeDiff24hPercent"`
 }
