73c05ef4b80a5bb9031cf5ab917d5f9d845a73ad
Guo Jing <jing.guo@bitdeer.com>
2021-07-05 10:55:14 +0800

    子账户部分接口

--- application/app/router.go
+++ application/app/router.go
@@ -15,7 +15,7 @@ func InitRouter(r *gin.Engine) error {
 	//// app相关
 	//InitAppRouter(r)
 	//// 子账户
-	//InitAccountSubaccount(r)
+	InitAccountSubaccount(r)
 	//// 矿机
 	//InitMiningWorker(r)
 	//// 报警
@@ -53,11 +53,12 @@ func InitAppRouter(r *gin.Engine) {
 
 /// 子账户相关
 func InitAccountSubaccount(r *gin.Engine) {
-	//pgroup := r.Group("/account/subaccount/")
+	pgroup := r.Group("/subaccount")
 	//// 子账户
-	//pgroup.GET("/list", controller.GetSubAccountList)
-	//pgroup.GET("/hashrate", controller.GetSubAccountHashrates)
-	//pgroup.GET("/changeHashrate", controller.ChangeSubAccountHashrate)
+	pgroup.GET("/list", controller.GetSubAccountList)
+	pgroup.GET("/info", controller.GetSubAccountInfo)
+	pgroup.GET("/hashrate", controller.GetSubAccountHashrates)
+	pgroup.GET("/changeHashrate", controller.ChangeSubAccountHashrate)
 	//// 收款地址管理
 	//pgroup.GET("/info", controller.GetAccountInfo)
 	//pgroup.GET("/addressPayset", controller.GetSubaccountPayset)
@@ -70,10 +71,9 @@ func InitAccountSubaccount(r *gin.Engine) {
 	//pgroup.GET("/mining/earnHistory", controller.GetEarnHistory)
 	//pgroup.GET("/mining/merge/earnStats", controller.GetMergeEarnstats)
 	//pgroup.GET("/mining/merge/earnHistory", controller.GetMergeEarnHistory)
-	//// 隐藏子账户
-	////pgroup.GET("/hidden/list", controller.GetSubaccountList)
-	//pgroup.GET("/hidden", controller.SetSubacountHiiden)
-	//pgroup.GET("/hiddenCancel", controller.CancelSubacountHiiden)
+	// 隐藏子账户
+	pgroup.GET("/hidden", controller.SetSubaccountHidden)
+	pgroup.GET("/hiddenCancel", controller.CancelSubaccountHidden)
 }
 
 /// 报警相关
--- application/service/account.go
+++ application/service/account.go
@@ -11,14 +11,14 @@ type accountHandler struct{}
 
 var AccountService = &accountHandler{}
 
-func (p *accountHandler) GetSubAccountList(c *gin.Context, puid string, isHidden int) ([]model.SubaccountEntity, error) {
+func (p *accountHandler) GetSubAccountList(c *gin.Context, puid string, isHidden int) ([]model.SubaccountEntity2, error) {
 	var algorithmsParams = map[string]interface{}{}
 	algorithmsParams["puid"] = puid
 	algorithmsParams["is_hidden"] = isHidden
 	algorithmsParams["is_guardian"] = 0
 	algorithmsParams["order_by"] = "puid"
 
-	var subaccountList = make([]model.SubaccountEntity, 0)
+	var subaccountList = make([]model.SubaccountEntity2, 0)
 	if subAccountAlgorithms, e := btcpoolclient.GetSubAccountAlgorithms(c, algorithmsParams); e != nil {
 		return subaccountList, e
 	} else {
@@ -27,7 +27,7 @@ func (p *accountHandler) GetSubAccountList(c *gin.Context, puid string, isHidden
 				j := 0
 				var l = make([]clientModel.SubAccountCoinEntity, 0)
 				for _, coinAccount := range algorithm.CoinAccounts {
-					if algorithm.IsSmart() == coinAccount.IsSmart() && coinAccount.IsHidden == isHidden{
+					if algorithm.IsSmart() == coinAccount.IsSmart() && coinAccount.IsHidden == isHidden {
 						l = append(l, coinAccount)
 						//subaccount.Algorithms[j] = algorithm
 						j++
@@ -38,35 +38,61 @@ func (p *accountHandler) GetSubAccountList(c *gin.Context, puid string, isHidden
 		}
 
 		for _, account := range subAccountAlgorithms.SubAccounts {
-			var subaccountEntity = model.SubaccountEntity{}
-			subaccountEntity.Title = account.Name+" "+account.RegionText
-			subaccountEntity.SearchKey = account.Name+" "+account.RegionText // TODO searchKey 中文情况下可以加拼音
-			subaccountEntity.Algorithms = make([]model.SubaccountAlgorithmEntity, 0)
+			var subaccountEntity = model.SubaccountEntity2{}
+			subaccountEntity.Title = account.Name + " " + account.RegionText
+			subaccountEntity.SearchKey = account.Name + " " + account.RegionText // TODO searchKey 中文情况下可以加拼音
+			subaccountEntity.SubAccount = make([]model.SubaccountAlgorithmCoinAccountEntity, 0)
 			for _, algorithm := range account.Algorithms {
-				var a = model.SubaccountAlgorithmEntity{}
-				a.AlgorithmText = algorithm.AlgorithmText
-				a.CurrentCoin = algorithm.CurrentCoin
-				a.CurrentPuid = algorithm.CurrentPuid
-				a.IsSmart = algorithm.IsSmart()
-				a.SupportCoins = algorithm.SupportCoins
-				a.SubAccount = make([]model.SubaccountAlgorithmCoinAccountEntity, 0)
+				//var a = model.SubaccountAlgorithmEntity{}
+				//a.AlgorithmText = algorithm.AlgorithmText
+				//a.CurrentCoin = algorithm.CurrentCoin
+				//a.CurrentPuid = algorithm.CurrentPuid
+				//a.IsSmart = algorithm.IsSmart()
+				//a.SupportCoins = algorithm.SupportCoins
+				//a.SubAccount = make([]model.SubaccountAlgorithmCoinAccountEntity, 0)
 				for _, coinAccount := range algorithm.CoinAccounts {
 					var coinA = model.SubaccountAlgorithmCoinAccountEntity{}
 					coinA.CoinType = coinAccount.CoinType
 					coinA.Puid = coinAccount.Puid
 					coinA.IsHidden = coinAccount.IsHidden == 1
 					coinA.IsCurrent = coinAccount.IsCurrent == 1
+					coinA.SupportCoins = algorithm.SupportCoins
 
-					a.SubAccount = append(a.SubAccount, coinA)
+					subaccountEntity.SubAccount = append(subaccountEntity.SubAccount, coinA)
 				}
 
-				subaccountEntity.Algorithms = append(subaccountEntity.Algorithms, a)
+				//subaccountEntity.Algorithms = append(subaccountEntity.Algorithms, a)
 			}
 
 			subaccountList = append(subaccountList, subaccountEntity)
 		}
 
 		return subaccountList, nil
+	}
+}
+func (p *accountHandler) GetCurrentSubAccount(c *gin.Context, puid string) (*model.SubaccountEntityCurrent2, error) {
+	if subaccountList, err := AccountService.GetSubAccountList(c, puid, 0); err != nil {
+		return nil, err
+	} else {
+		for _, coinAccount := range subaccountList {
+			for _, a := range coinAccount.SubAccount {
+				if a.Puid == puid {
+					entity := model.SubaccountEntityCurrent2{
+						Puid:     a.Puid,
+						CoinType: a.CoinType,
+					}
+					entity.SubaccountEntity2 = coinAccount
+					return &entity, nil
+				}
+			}
+		}
+
+		entity := model.SubaccountEntityCurrent2{
+			Puid:     subaccountList[0].SubAccount[0].Puid,
+			CoinType: subaccountList[0].SubAccount[0].CoinType,
+		}
+		entity.SubaccountEntity2 = subaccountList[0]
+		return &entity, nil
 	}
 }
 
@@ -83,7 +109,7 @@ func (p *accountHandler) GetSubAccountHashrates(c *gin.Context, puids string) (m
 			hashrateEntity.WorkerTotal = h.Workers
 			hashrateEntity.WorkerActive = h.WorkersActive
 			hashrateEntity.Hashrate = h.Shares1d
-			hashrateEntity.HashrateUnit = h.Shares1dUnit+h.HashrateSuffix
+			hashrateEntity.HashrateUnit = h.Shares1dUnit + h.HashrateSuffix
 			hashrateEntity.LastAlertTrans = h.LatestAlert.Trans
 
 			data[puid] = hashrateEntity
